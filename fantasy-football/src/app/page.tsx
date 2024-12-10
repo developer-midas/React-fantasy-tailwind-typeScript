a64db8b7e03b1421aff11f2c8e3ae602a0b2f443
developer-midas <andreshoberman1130@gmail.com>
2024-12-10 05:01:08 -0500

    modify responsive

--- fantasy-football/src/app/page.tsx
+++ fantasy-football/src/app/page.tsx
@@ -5,7 +5,7 @@ import Table from "./components/Table";
 export default function Home() {
   const items = ["Option 1", "Option 2", "Option 3", "Option 4"];
   return (
-    <div className="bg-black bg-opacity-90 text-white text-opacity-90 w-[1440px] mx-auto">
+    <div className="bg-black bg-opacity-90 text-white text-opacity-90 container mx-auto md:w-full">
       <header className="p-6 flex gap-4 flex-start items-center bg-black h-24">
         <Image
           className="w-[48px] h-[41px]"
@@ -21,17 +21,18 @@ export default function Home() {
       {/* Body */}
       <div className="flex justify-center bg-[#1F1F1F] flex-col">
         <div className="py-12 px-36 flex-row">
-          <div className="p-8 bg-white bg-opacity-10 flex items-center gap-8 rounded-lg">
+          <div className="p-8 bg-white bg-opacity-10 flex flex-wrap justify-center items-center gap-8 rounded-lg">
             <DropDown items={items} />
             <DropDown items={items} />
             <DropDown items={items} />
           </div>
         </div>
-        <div className="flex flex-row gap-x-10 w-full px-10 pb-5">
-          <div className="flex flex-1 md:overflow-hidden overflow-scroll">
+        <div className="flex lg:flex-row lg:justify-start lg:items-stretch md:flex-col md:justify-center md:items-center 
+                          sm:flex-col sm:justify-start sm:items-center flex-col justify-start items-center gap-x-10 h-full w-full px-10 pb-5">
+          <div className="flex lg:flex-1 md:overflow-hidden sm:overflow-scroll">
             <Table />
           </div>
-          <div className="flex flex-col h-full rounded-lg overflow-hidden bg-black">
+          <div className="flex flex-col h-full rounded-lg overflow-hidden bg-black w-[400px]">
             <Image src="/avatar.png" alt="icon" width={400} height={256} />
             <div className="bg-[#2F2F2F] text-center text-white text-opacity-80 py-4">
               <p className="text-4xl">Tom Brady</p>
